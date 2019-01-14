ebd176497131ee98bf74b9b3eca19b41b2af6b01
Michal Adamkiewicz <mikh3x4@gmail.com>
2019-01-14 13:16:36 -0800

    strucutre

--- arm_controller.py
+++ arm_controller.py
@@ -5,6 +5,16 @@ from roboclaw_interface import RoboClaw
 import math
 
 
+FIRST_LINK = 1000
+SECOND_LINK = 1000
+
+# native angles = 0 at extension
+
+def find_serial_port():
+    return '/dev/ttyUSB0'
+    return '/dev/ttyACM0'
+    return '/dev/tty.usbmodem1141'
+
 class Vector(list):
 
     def __add__(self,other):
@@ -31,16 +41,30 @@ class Arm:
     def __init__(self):
         self.target = Subscriber(8410)
 
+        self.xyz_names = ["x", "y"]
+
+        self.motor_names = ["shoulder",
+                            "elbow"]
+
+        self.native_positions = { motor:0 for motor in self.motor_names}
+        self.CPR = { motor:100 for motor in self.motor_names}
+
+        self.rc = RoboClaw(find_serial_port(), names = self.motor_names, addresses = [128] ) # addresses = [128, 129, 130])
 
 
     def get_location(self):
-        pass
+        for motor in self.motor_names:
+            self.native_positions[motor] = self.rc.get_encoder(motor)/self.CPR[motor]
+
+        self.xyz_positions = self.native_to_xyz(self.native_positions)
 
     def xyz_to_native(self, xyz):
         pass
 
     def native_to_xyz(self, native):
         pass
+        # self.native_positions['x'] = 
+        # self.native_positions['y'] = 
 
     def update(self):
         current_location = Vector(self.get_location())
